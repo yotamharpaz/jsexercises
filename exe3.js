1b2764df87ff2683e2a1ade0e02f2bedc7aae2bd
yotamharpaz <89574011+yotamharpaz@users.noreply.github.com>
2021-08-29 20:49:04 +0300

    exercsis3

---
+++ exe3.js
@@ -0,0 +1,16 @@
+
+const num = -8
+let absolute ;
+function abs(num) {
+    if(num > 0){
+        return num
+    }
+    if(num === 0){
+        return 0
+    }
+    if(num< 0) {
+        return -num
+    }
+}
+
+console.log(abs(-8))
